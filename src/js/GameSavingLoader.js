68415c67e3f4f00489f4b11aeca0bc33d70f7b3a
SergeiSeledkov <gthemail92@gmail.com>
2022-03-03 20:48:05 +0500

    v2

--- src/js/GameSavingLoader.js
+++ src/js/GameSavingLoader.js
@@ -1,9 +1,12 @@
 import parser from './parser';
 import reader from './reader';
+import GameSaving from './GameSaving';
 
 export default class GameSavingLoader {
   static async load() {
     const responseRead = await reader();
-    return parser(responseRead);
+    const responseParse = await parser(responseRead);
+    const result = await new GameSaving(JSON.parse(responseParse));
+    return result;
   }
 }
